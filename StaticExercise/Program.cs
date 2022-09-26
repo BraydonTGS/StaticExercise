f2179acaefe34c83500ca65b46ec484b381ebb4a
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-26 11:58:57 -0500

    created a class with two static methods and calling from main

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using StaticExercise;
+
 Console.WriteLine("Hello, World!");
 
+var fTemp = TempConverter.FahrenheitToCelsius(32.00);
+var cTemp = TempConverter.CelsiusToFahrenheit(32.00);
+
+Console.WriteLine(fTemp);
+Console.WriteLine(cTemp);
+Console.ReadKey();
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+    public class TempConverter
+    {
+        public TempConverter()
+        {
+        }
+
+        public static double FahrenheitToCelsius(double number)
+        {
+            return 5 / 9 * (number - 32);
+        }
+
+        public static double CelsiusToFahrenheit(double number)
+        {
+            return (number * 9 / 5) + 32;
+        }
+    }
+}
+
